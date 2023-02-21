86d3136c8febea065b2f96e86a9f7fdffb4573a1
Mateusz Jablonski <mjablonski.praca@gmail.com>
2023-02-21 12:43:31 +0100

    changed audio script favicon and minor misc

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 // I. Playing on/off audio on button play-button -->
 // 1. DOM elements variables
-let playButton = document.getElementById('play-button');
-let playIcon = document.getElementById('play-icon');
+const playIcon = document.getElementById('play-icon');
+const audioElement = document.getElementById('audio');
 
-// 2. deklaracja funkcji warunkowej tworzącej i obsługującej plik audio;
-const audioHandler = () => {
-	if (document.getElementById('audio')) {
-		audio.pause();
-		playButton.removeChild(audio);
-	} else {
-		let audio = document.createElement('audio');
-		audio.setAttribute('id', 'audio');
-		audio.setAttribute('src', './audio/id-playbtn-song1.mp3');
-		playButton.appendChild(audio);
-		audio.play();
-	}
-};
-
-// 3. onClick - switching icons using contains() + handling audio
-playButton.addEventListener('click', () => {
-	if (playIcon.classList.contains('icon-play3')) {
+// 2. onClick - switching icons using contains() + handling audio
+const playMusic = () => {
+	if (audioElement.paused) {
 		playIcon.classList.remove('icon-play3');
 		playIcon.classList.add('icon-pause2');
-		audioHandler();
+		audioElement.play();
 	} else {
 		playIcon.classList.remove('icon-pause2');
 		playIcon.classList.add('icon-play3');
-		audioHandler();
+		audioElement.pause();
 	}
-});
+};
 // <-- I. Playing on/off audio on button play-button
